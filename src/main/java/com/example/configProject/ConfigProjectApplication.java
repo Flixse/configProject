0e79bde0a43327a5e66ed4ebdc3319f707dab80e
yd95pd <felix.porres@ing.com>
2018-12-17 10:45:40 +0100

    This is the first commit -- > added the packagaes

---
+++ src/main/java/com/example/configProject/ConfigProjectApplication.java
@@ -0,0 +1,14 @@
+package com.example.configProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigProjectApplication.class, args);
+	}
+
+}
+
